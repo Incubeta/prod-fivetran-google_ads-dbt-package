2d8eb6e033bcfdfc8f1178d29c7b31702d933314
Chigozie Boniface <97526590+chigozieboniface@users.noreply.github.com>
2024-11-11 12:16:55 +0200

    update add_custom_fields.sql

--- macros/add_custom_fields.sql
+++ macros/add_custom_fields.sql
@@ -2,9 +2,6 @@
   {%- if var('googleads_custom_fields', None) is not none -%}
     {%- set custom_fields = var('googleads_custom_fields').split(',') -%}
     {%- for field in custom_fields -%}
-      {%- if field == "conversions_value" -%}
-      SAFE_CAST({{ field }} AS STRING) AS Total_conversion_value,
-      {%- endif -%}
       SAFE_CAST({{ field }} AS STRING) AS {{ field }},
     {%- endfor -%}
   {%- endif -%}
